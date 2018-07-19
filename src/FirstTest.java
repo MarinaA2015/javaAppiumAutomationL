d6fd27d71696824714f84bb2dcdc37a825e97342
MarinaA2015 <m.abrashkina2015@gmail.com>
2018-07-19 14:51:10 +0300

    new comment

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -1,9 +1,14 @@
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.net.URL;
 
@@ -36,7 +41,136 @@ public class FirstTest {
     }
 
     @Test
-    public void firstTest(){
-        System.out.println("First test run");
+    public void firstTest()
+    {
+        waitForElementAndClick(By.xpath("//*[contains(@text,'Search Wikipedia')]"),
+                                "Cannot find Search Wikipedia input",
+                                5);
+
+        waitForElementAndSendKeys(By.xpath("//*[contains(@text,'Search…')]"),
+                                 "Java",
+                                 "Cannot find object-oriented programming language topic searching by Java",
+                                15);
+
+
+        waitForElementPresent(By.xpath("//*[@resource-id = 'org.wikipedia:id/page_list_item_container']//*[@text = 'Object-oriented programming language']"),
+                                    "Cannot find object-oriented programming language topic searching by Java",
+                                        15);
+
+    }
+
+    @Test
+    public void testCancelSearch()
+    {
+        waitForElementAndClick(By.id("org.wikipedia:id/search_container"),
+                                "Cannot find Search Wikipedia input",
+                                5);
+
+        waitForElementAndSendKeys(By.id("org.wikipedia:id/search_src_text"),
+                                  "Java",
+                                    "Cannot find Search.. input",
+                                    5);
+
+        waitForElementPresent(By.xpath("//*[@resource-id = 'org.wikipedia:id/page_list_item_container']//*[@text = 'Object-oriented programming language']"),
+                             "Cannot find object-oriented programming language topic searching by Java",
+                                15);
+
+        waitForElementAndClear(By.id("org.wikipedia:id/search_src_text"),
+                                "Cannot find Search.. input",
+                                 15);
+
+        waitForElementAndClick(By.id("org.wikipedia:id/search_close_btn"),
+                                "Cannot find X - button",
+                                5);
+
+        waitForElementNotPresent(By.id("org.wikipedia:id/search_close_btn"),
+                                      "X - button is still on the page",
+                                        15);
+    }
+
+    @Test
+    public void testCompareArticleTitle()
+    {
+        waitForElementAndClick(By.id("org.wikipedia:id/search_container"),
+                "Cannot find Search Wikipedia input",
+                5);
+
+        waitForElementAndSendKeys(By.id("org.wikipedia:id/search_src_text"),
+                "Java",
+                "Cannot find Search.. input",
+                5);
+
+        waitForElementAndClick(By.xpath("//*[@resource-id = 'org.wikipedia:id/page_list_item_container']//*[@text = 'Object-oriented programming language']"),
+                "Cannot find object-oriented programming language topic searching by Java",
+                15);
+
+        WebElement element = waitForElementPresent(By.id("org.wikipedia:id/view_page_title_text"),
+            "Cannot find article title",
+            15);
+        String title = element.getAttribute("text");
+
+        Assert.assertEquals("We see unexpected title",
+                        "Java (programming language)",
+                            title);
+    }
+
+    @Test
+    public void testTextSearchPresentInSearchField(){
+        waitForElementAndClick(By.id("org.wikipedia:id/search_container"),
+                "Cannot find Search Wikipedia input",
+                5);
+
+        WebElement element = waitForElementPresent(By.id("org.wikipedia:id/search_src_text"),
+                "Cannot find Search.. input",
+                15);
+
+        String textInSearchField = element.getAttribute("text");
+
+        Assert.assertEquals("We do not see 'Search..' text",
+                "Search…",
+                textInSearchField);
+    }
+
+    private WebElement waitForElementPresent(By by, String errorMessage, long timeoutInSeconds)
+    {
+        WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
+        wait.withMessage(errorMessage + "\n");
+        //By by  = By.xpath(xpath);
+        return wait.until(ExpectedConditions.presenceOfElementLocated(by));
+    }
+
+    private WebElement waitForElementPresent(By by, String errorMessage)
+    {
+        return waitForElementPresent(by, errorMessage, 5);
+    }
+
+    private WebElement waitForElementAndClick(By by, String errorMessage, long timeoutInSeconds)
+    {
+        WebElement element = waitForElementPresent(by, errorMessage,timeoutInSeconds);
+        element.click();
+        return element;
+    }
+
+    private WebElement waitForElementAndSendKeys(By by, String value, String errorMessage, long timeoutInSeconds)
+    {
+        WebElement element = waitForElementPresent(by, errorMessage,timeoutInSeconds);
+        element.sendKeys(value);
+        return element;
+    }
+
+
+    private boolean waitForElementNotPresent(By by, String errorMessage, long timeoutInSeconds)
+    {
+        WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
+        wait.withMessage(errorMessage + "\n");
+        //By by = By.id(id);
+        return wait.until(ExpectedConditions.invisibilityOfElementLocated(by));
+    }
+
+    private WebElement waitForElementAndClear(By by, String errorMessage, long timeoutInSeconds)
+    {
+        WebElement element = waitForElementPresent(by, errorMessage,timeoutInSeconds);
+        element.clear();
+        return element;
     }
 }
